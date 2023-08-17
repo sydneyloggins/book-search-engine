693bc68df856a7b4ec9c85ae08ee37a8277d2c74
Sydney Loggins <sydneyloggins@Sydneys-Mini.attlocal.net>
2023-08-17 11:16:17 -0500

    add typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,46 @@
-const resolvers = require('./resolvers');
-const typeDefs = require('./typeDefs');
+const { gql } = require('apollo-server-express');
 
-module.exports = { resolvers, typeDefs };
+//typeDefs
+const typeDefs = gql`
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        forSale: String
+        link: String
+        title: String
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Query {
+        me: User
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    input SavedBookInput {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        forSale: String
+        link: String
+        title: String
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: SavedBookInput!): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+module.exports = typeDefs;
